bbebfc1b3487fc35ea04a81c49f2ed6cf1086e2e
minkukjo <strike0115@naver.com>
2023-11-19 18:26:37 +0900

    Abstract result view

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:bookamera/view/result.dart';
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 
@@ -70,7 +71,7 @@ class TakePictureScreenState extends State<TakePictureScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: EmptyAppBar(),
+      appBar: const EmptyAppBar(),
       // You must wait until the controller is initialized before displaying the
       // camera preview. Use a FutureBuilder to display a loading spinner until the
       // controller has finished initializing.
@@ -83,8 +84,6 @@ class TakePictureScreenState extends State<TakePictureScreen> {
             final mediaSize = MediaQuery.of(context).size;
             final scale =
                 1 / (_controller.value.aspectRatio * mediaSize.aspectRatio);
-
-            _controller.setFlashMode(FlashMode.off);
 
             return ClipRect(
               clipper: _MediaSizeClipper(mediaSize),
@@ -109,6 +108,8 @@ class TakePictureScreenState extends State<TakePictureScreen> {
             // Ensure that the camera is initialized.
             await _initializeControllerFuture;
 
+            _controller.setFlashMode(FlashMode.off);
+
             // Attempt to take a picture and get the file `image`
             // where it was saved.
             final image = await _controller.takePicture();
@@ -118,7 +119,7 @@ class TakePictureScreenState extends State<TakePictureScreen> {
             // If the picture was taken, display it on a new screen.
             await Navigator.of(context).push(
               MaterialPageRoute(
-                builder: (context) => DisplayPictureScreen(
+                builder: (context) => ResultScreen(
                   // Pass the automatically generated path to
                   // the DisplayPictureScreen widget.
                   imagePath: image.path,
@@ -149,22 +150,5 @@ class _MediaSizeClipper extends CustomClipper<Rect> {
   @override
   bool shouldReclip(CustomClipper<Rect> oldClipper) {
     return true;
-  }
-}
-
-// A widget that displays the picture taken by the user.
-class DisplayPictureScreen extends StatelessWidget {
-  final String imagePath;
-
-  const DisplayPictureScreen({super.key, required this.imagePath});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      // The image is stored as a file on the device. Use the `Image.file`
-      // constructor with the given path to display the image.
-      body: Image.file(File(imagePath)),
-    );
   }
 }
---
+++ lib/view/result.dart
@@ -0,0 +1,19 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+
+class ResultScreen extends StatelessWidget {
+  final String imagePath;
+
+  const ResultScreen({super.key, required this.imagePath});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      // The image is stored as a file on the device. Use the `Image.file`
+      // constructor with the given path to display the image.
+      body: Image.file(File(imagePath)),
+    );
+  }
+}
